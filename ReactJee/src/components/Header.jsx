b565ee33e342584b2654ba67905667f72bc79027
loyarabhishek92 <loyarabhishek92@gmail.com>
2026-03-10 17:39:33 +0545

    rtk created

--- ReactJee/src/components/Header.jsx
+++ ReactJee/src/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
   return (
     <div className="flex justify-between p-5 bg-blue-100">
       <Button><NavLink to={'/'}>Go to Home</NavLink></Button>
-      <h1>Formik</h1>
+      <h1>RTK</h1>
        <Button><NavLink to={'/addForm'}>Add</NavLink></Button>
         
       
--- ReactJee/src/pages/home/Home.jsx
+++ ReactJee/src/pages/home/Home.jsx
@@ -1,40 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { useSelector } from "react-redux"
 
 export default function Home() {
-  const { users } = useSelector((state) => state.userSlice);
   return (
-    <div className="px-10 mt-5 grid grid-cols-4 gap-3">
-       
-      {
-        users.map((user) => {
-          return <Card key={user.id} size="sm" className=" w-full max-w-sm shadow-xl hover:bg-blue-50">
-      <CardHeader>
-        <CardTitle>{user.username}</CardTitle>
-        <CardDescription>
-          {user.email}
-        </CardDescription>
-        <CardDescription>
-          {user.gender}
-        </CardDescription>
-        <CardDescription>
-          {user.country}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <p>
-         { user.detail}
-        </p>
-      </CardContent>
-      <CardFooter>
-        <Button variant="default" size="sm" className="w-full">
-          Action
-        </Button>
-      </CardFooter>
-    </Card>
-        })
-      }
+    <div>
+      home
     </div>
   )
 }
